b218113151d04076576dbb02e0ad688452387b4d
Woshishui 1133 <1132170643@qq.com>
2019-11-29 19:47:05 +0800

    12000

--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -1,20 +1,16 @@
 <template>
   <div>
     <div>登录页面</div>
-    <div>
-      www
-    </div>
+    <div>www</div>
   </div>
 </template>
 
 <script>
-  export default {
-    created() {
-      console.log(this.$store.state.token)
-    },
+export default {
+  created() {
+    console.log(this.$store.state.token);
   }
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
